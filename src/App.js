3c78671e9fe2118bab57772bf585f186996a702d
rubeneezy <rpachecocaldera@gmail.com>
2024-03-10 20:33:08 -0600

    added race results for saudi arabia

--- src/App.js
+++ src/App.js
@@ -10,28 +10,35 @@ function App() {
   const raceResults = [
     { name: "Fern(ando)", race: "Bahrain", points: 25 },
     { name: "Katie", race: "Bahrain", points: 55 },
-    { name: "carmen", race: "Bahrain", points: 43 },
+    { name: "Carmen", race: "Bahrain", points: 43 },
     { name: "RUENDY", race: "Bahrain", points: 43 },
     { name: "Ruben", race: "Bahrain", points: 51 },
     { name: "Luiis", race: "Bahrain", points: 43 },
     { name: "Alex Ramirez", race: "Bahrain", points: 37 },
-  ];
+    { name: "Katie", race: "Saudi Arabia", points: 58 },
+    { name: "Carmen", race: "Saudi Arabia", points: 62 },
+    { name: "RUENDY", race: "Saudi Arabia", points: 25 },
+    { name: "Ruben", race: "Saudi Arabia", points: 66 },
+    { name: "Luiis", race: "Saudi Arabia", points: 25 },
+    { name: "Alex Ramirez", race: "Saudi Arabia", points: 66 }
+];
+
 
   const totalStandings = [
-    { name: "Katie", points: 55 },
-    { name: "Ruben", points: 51 },
-    { name: "carmen", points: 43 },
-    { name: "RUENDY", points: 43 },
-    { name: "Luiis", points: 43 },
-    { name: "Alex Ramirez", points: 37 },
+    { name: "Katie", points: 113 },
+    { name: "Ruben", points: 118 },
+    { name: "Carmen", points: 106 },
+    { name: "RUENDY", points: 68 },
+    { name: "Luiis", points: 68 },
+    { name: "Alex Ramirez", points: 104 },
     { name: "Fern(ando)", points: 25 },
   ];
 
   return (
     <div className="App">
       <Header /> 
+      <TotalStandings standings={totalStandings.sort((a, b) => a.points > b.points)} />
       <RaceResults results={raceResults} />
-      <TotalStandings standings={totalStandings} />
     </div>
   );
 }
